1fe53c3fc7a49773fe0cb99a988a55a2af2fd8a0
Mariusz Bałęczny <mariuszbaleczny@gmail.com>
2022-01-22 12:36:14 +0100

    feat: implement calculating merkle tree root for data read from file

--- lib/merkle_tree_root.ex
+++ lib/merkle_tree_root.ex
@@ -1,18 +1,18 @@
 defmodule MerkleTreeRoot do
-  @moduledoc """
-  Documentation for `MerkleTreeRoot`.
-  """
+  def calculate_root_from_file(file_path) when is_binary(file_path) do
+    if File.exists?(file_path) do
+      file_path
+      |> read_file_content()
+      |> MerkleTreeRoot.CalculateRoot.call()
+    else
+      raise ArgumentError, message: "File #{file_path} does not exists."
+    end
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MerkleTreeRoot.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp read_file_content(file_path) do
+    file_path
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.to_list()
   end
 end
--- test/merkle_tree_root_test.exs
+++ test/merkle_tree_root_test.exs
@@ -1,8 +1,27 @@
 defmodule MerkleTreeRootTest do
   use ExUnit.Case
-  doctest MerkleTreeRoot
+  import Mox
 
-  test "greets the world" do
-    assert MerkleTreeRoot.hello() == :world
+  setup opts do
+    # set production implementation of crypto module
+    stub_with(MerkleTreeRoot.MockCrypto, MerkleTreeRoot.Crypto.Sha256)
+
+    verify_on_exit!(opts)
+  end
+
+  describe "calculate_root_from_file/1" do
+    test "returns calculated root for data read from file" do
+      file_path = "input.txt"
+
+      assert MerkleTreeRoot.calculate_root_from_file(file_path)
+    end
+
+    test "returns ArgumentError when file does not exists" do
+      file_path = "not_existing_file"
+
+      assert_raise ArgumentError, "File #{file_path} does not exists.", fn ->
+        MerkleTreeRoot.calculate_root_from_file(file_path)
+      end
+    end
   end
 end
